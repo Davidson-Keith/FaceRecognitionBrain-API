e5e9297581966bccc21d12e24674368c0ec3f2ce
Keith Davidson <keith.davidson@griffithuni.edu.au>
2023-04-05 18:38:47 +1000

    Fix port

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ const image = require('./controllers/image');
 const saltRounds = 10; // for bcrypt
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
