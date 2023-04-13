a1bf2c4b2b2390af662f7900bf290ef4414006c7
Keith Davidson <keith.davidson@griffithuni.edu.au>
2023-04-13 17:33:44 +1000

    Allow CORS.

--- server.js
+++ server.js
@@ -28,13 +28,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+
+// app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
 
 console.log(process.env);
+
 
 // -------------------
 //
